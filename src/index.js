53f1085e85eff9bc69f073049d087ee6200521f1
Mohammed El Saka <mel-saka@outlook.com>
2024-11-04 00:19:03 +1300

    improved wheel's style

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/App.css';
 import App from './App';
+import 'primeicons/primeicons.css';
+
 
 ReactDOM.render(
   <React.StrictMode>
